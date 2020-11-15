beb17f41169176c62258f7417c4cff6979d3409a
d9nich <dan.galaiko@gmail.com>
2020-11-15 22:52:53 +0200

    Hot pizza now ready to go with new server

--- js/getJson.js
+++ js/getJson.js
@@ -1,9 +1,9 @@
-const SERVER_IP = '34.227.61.134';
-export const promisedProducts = getObj(`http://${SERVER_IP}/getProducts`);
-export const promisedProductsCategories = getObj(`http://${SERVER_IP}/getCategories`);
-export const promisedIngredients = getObj(`http://${SERVER_IP}/getIngredients`);
-export const promisedRecommendations = getObj(`http://${SERVER_IP}/getRecommended`);
-export const promisedPromotions = getObj(`http://${SERVER_IP}/getPromotions`);
+const SERVER_IP = 'https://hotpizza.ml';
+export const promisedProducts = getObj(`${SERVER_IP}/getProducts`);
+export const promisedProductsCategories = getObj(`${SERVER_IP}/getCategories`);
+export const promisedIngredients = getObj(`${SERVER_IP}/getIngredients`);
+export const promisedRecommendations = getObj(`${SERVER_IP}/getRecommended`);
+export const promisedPromotions = getObj(`${SERVER_IP}/getPromotions`);
 
 function getObj(url) {
     return fetch(url).then(data => data.json());
