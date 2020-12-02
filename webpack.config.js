0ce54a9fa75ee79992058ba5a2f9f5a697b36a58
d9nich <dan.galaiko@gmail.com>
2020-12-02 15:23:52 +0200

    fixed typo

--- webpack.config.js
+++ webpack.config.js
@@ -47,7 +47,7 @@ const config = {
         new CopyPlugin({
             patterns: [
                 {
-                    from: 'staic',
+                    from: 'static',
                 },
             ],
         }),
