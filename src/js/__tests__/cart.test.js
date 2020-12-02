5d35830114dc7622ed4ea799ccca139d0d3776c9
d9nich <dan.galaiko@gmail.com>
2020-12-02 20:12:11 +0200

    Basic test for cart.js has been written

--- src/js/__tests__/cart.test.js
+++ src/js/__tests__/cart.test.js
@@ -1,19 +1,53 @@
-beforeAll(() => {
+import './setup.test';
+import { CART } from '../cart';
+
+beforeEach(() => {
     document.body.innerHTML = `<div>
 <header><a id="cart"><span id="numberOfCartItems">00</span></a></nav></header>
 <main></main>
 </div>`;
+    CART.clearCart();
 });
-import './setup.test';
-import { CART } from '../cart';
 
 require('babel-polyfill');
 
 jest.mock('../getJson');
 
 test('test adding ability', () => {
-    document.getElementsByTagName('main')[0];
-    require('../renderHelp');
+    expect(CART.calculateNumberOfItemsInCart()).toBe(0);
     CART.addToCart('banana', 1);
     expect(CART['banana']).toBe(1);
+    expect(CART.calculateNumberOfItemsInCart()).toBe(1);
+    CART.addToCart('foo', 'qwe');
+    expect(CART.calculateNumberOfItemsInCart()).toBe(1);
+});
+
+test('test clear of cart', () => {
+    expect(CART.calculateNumberOfItemsInCart()).toBe(0);
+    CART.addToCart('banana', 1);
+    CART.clearCart();
+    expect(CART.calculateNumberOfItemsInCart()).toBe(0);
+    expect(CART['banana']).toBe(undefined);
+});
+
+test('test isEmpty', () => {
+    expect(CART.isEmpty()).toBeTruthy();
+    CART.addToCart('banana', 1);
+    expect(CART.isEmpty()).toBeFalsy();
+    CART.clearCart();
+    expect(CART.isEmpty()).toBeTruthy();
+});
+
+test('test remove from cart', () => {
+    CART.addToCart('banana', 1);
+    CART.addToCart('apple', 2);
+    expect(CART.calculateNumberOfItemsInCart()).toBe(3);
+    CART.removeFromCart('banana');
+    expect(CART.calculateNumberOfItemsInCart()).toBe(2);
+});
+
+test('test number of items', () => {
+    expect(CART.calculateNumberOfItemsInCart()).toBe(0);
+    CART.addToCart('banana', 1);
+    expect(CART.calculateNumberOfItemsInCart()).toBe(1);
 });
